82fe3d84b80389e681eb7c93ee50ecb0f992cd76
Boris-The-Slav <75560716+Boris-The-Slav@users.noreply.github.com>
2021-10-08 17:38:00 +0200

    added working icons

--- pages/[academyId]/[studentProgramId].js
+++ pages/[academyId]/[studentProgramId].js
@@ -40,7 +40,19 @@ const AcademyPage = props => {
       <main className={classes.academiesMain}>
         <Link href={`/`}>
           <div className={classes.backDiv}>
-            <i className="fas fa-chevron-left"></i>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-chevron-left"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"
+              />
+            </svg>
             <p className={classes.backToHome}>Back to home</p>
           </div>
         </Link>
